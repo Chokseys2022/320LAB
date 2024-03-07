18be8878c45ae078dab0bc202fc6405be6a20c3f
Chokseys2022 <choksey2022@gmail.com>
2024-03-07 15:02:04 -0800

    2nd

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,23 @@ import { useState } from "react";
 import MovieDisplay from "./components/MovieDisplay";
 import Form from "./components/Form";
 import "./App.css";
+import axios from 'axios';
+
+async function search(searchTerm) {
+  try {
+    let res = await axios.get(
+      `http://www.omdbapi.com/?apikey=${
+        import.meta.env.VITE_API_Key
+      }&t=${searchTerm}`
+    );
+    console.log(res);
+    setMovieData(res);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+
 function App() {
   const [MovieData, setMovieData] = useState(0);
   return (
@@ -12,6 +29,23 @@ function App() {
     </>
   );
 }
+
+
+
 export default App;
 
 //console.log(import.meta.env.VITE_API_Key);
+
+// async function search(searchTerm) {
+//   try {
+//     let res = await axios.get(
+//       `http://www.omdbapi.com/?apikey=${
+//         import.meta.env.VITE_API_Key
+//       }&t=${searchTerm}`
+//     );
+//     console.log(res);
+//     setMovieData(res);
+//   } catch (err) {
+//     console.error(err);
+//   }
+// }
